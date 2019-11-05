2d94dbe5b93cbd72146534a1bcf5ac3bc09dc384
Therese Cederholm <therese.cederholm@outlook.com>
2019-11-05 13:46:50 +0100

    Export()

--- GruppL_IK073G_ht19/Controllers/PersonsController.cs
+++ GruppL_IK073G_ht19/Controllers/PersonsController.cs
@@ -130,5 +130,22 @@ namespace GruppL_IK073G_ht19.Controllers
             }
             base.Dispose(disposing);
         }
+
+        //Exportera till PDF
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string GridHtml)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                StringReader sr = new StringReader(GridHtml);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+            }
+        }
     }
 }
