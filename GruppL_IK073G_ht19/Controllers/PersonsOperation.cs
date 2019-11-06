3be1d4d5fdbe0c071db6e309deb3d11fd1b6922e
Sara Lindbeck <sali1809@student.miun.se>
2019-11-06 10:17:20 +0100

    VievModel

---
+++ GruppL_IK073G_ht19/Controllers/PersonsOperation.cs
@@ -0,0 +1,24 @@
+﻿using GruppL_IK073G_ht19.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GruppL_IK073G_ht19.Controllers
+{
+    public class PersonsOperation
+    {
+        private gruppldbEntities1 db = new gruppldbEntities1();
+
+        List<Employments> employments = new List<Employments>();
+
+        public List <Employments> FindEmployment(int? id)
+        {
+            employments = db.Employments.ToList();
+
+            var employmentsa = employments.Where(s => s.Person_Id == id).ToList();
+            return employmentsa;
+
+        }
+    }
+}
